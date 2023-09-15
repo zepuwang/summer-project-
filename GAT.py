fb611939589fcf45088dcfdd93eb1a39e86eded2
zepuwang <123119171+zepuwang@users.noreply.github.com>
2023-09-14 22:41:03 -0400

    Update GAT.py

--- GAT.py
+++ GAT.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from layers import GraphAttentionLayer  # , SpGraphAttentionLayer
+from layers import GraphAttentionLayer
 
 
 class GAT(nn.Module):
